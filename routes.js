14d09a68afcaa607199f4a69ed172746b3f5c146
Matt Finucane <matfin@gmail.com>
2015-04-14 12:22:35 +0200

    Fleshing out templates, controllers and routes.

--- routes.js
+++ routes.js
@@ -1,3 +1,26 @@
 /**
- *	
+ *	Configure the loading template
  */
+Router.configure({
+	loadingTemplate: 'components_loading'
+});
+
+/**
+ *	Router mapping - setting up the views and components
+ */
+Router.map(function() {
+
+	/**
+	 *	Landing page - client side routing
+	 */
+	this.route('work', {
+		path: '/',
+		template: 'main',
+		yieldTemplates: {
+			'components_header': 	{to: 'header'},
+			'views_work': 			{to: 'content'}
+		}
+
+	}, {where: 'client'});
+
+});
