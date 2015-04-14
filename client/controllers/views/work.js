82c7ba9531d9e7f4ce894f78158485213050a8cc
Matt Finucane <matfin@gmail.com>
2015-04-14 17:16:52 +0200

    Basic styling for the portfolio item view.

---
+++ client/controllers/views/work.js
@@ -0,0 +1,30 @@
+/**
+ *	Template - views_work
+ *	Callback function called automatically when the template has been created
+ *
+ *	@method created
+ */
+Template['views_work'].created = function() {
+	console.log('Loaded views_work');
+};
+
+/**
+ *	Template - views_work
+ *	Callback function called automatically when the template has been rendered
+ *
+ *	@method rendered
+ */
+Template['views_work'].rendered = function() {
+	
+};
+
+/**
+ *	Template - views_work
+ *	Callback function called automatically when the template has been destroyed
+ *
+ *	@method destroyed
+ */
+Template['views_work'].destroyed = function() {
+	
+};
+
