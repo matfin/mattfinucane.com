8a256861286959635cace3f240a6a9910f914a57
Matt Finucane <matfin@gmail.com>
2015-08-11 16:29:55 +0200

    Added meteor velocity and jasmine for testing and created skeleton spec for portfolio view controller.

---
+++ tests/jasmine/client/unit/views/portfolioSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Template.views_portfolio', function() {
+
+	describe('created', function() {
+
+		it('should work', function() {
+			expect(true).toBe(true);
+		});
+
+	});
+
+});
