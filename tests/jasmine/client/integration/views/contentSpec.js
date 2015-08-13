d0d645162974bc09a4946cc970d2ccdd019fcc3d
Matt Finucane <matfin@gmail.com>
2015-08-14 00:07:14 +0200

    Testing helpers for the content item template.

--- tests/jasmine/client/integration/views/contentSpec.js
+++ tests/jasmine/client/integration/views/contentSpec.js
@@ -89,6 +89,88 @@ describe('views_content', function() {
 			 */
 			setTimeout(done, 50);
 		})
+	});
+
+	describe('helpers', function() {
+
+		describe('groupedContentItems', function() {
+
+			it('it should return the correct combination of content items', function(done) {
+				
+				/**
+				 *	Spies
+				 */
+				spyOn(Meteor, 'subscribe').and.returnValue({
+					subscriptionId: 3,
+					ready: function() {
+						return true;
+					}
+				});
+
+				spyOn(Core.app.collections.entries, 'find').and.returnValue({
+					fetch: function() {
+						return [
+							{
+								fields: {
+									isStandalone: true,
+									title: 'Some Content',
+									images: [],
+									order: 1
+								}
+							},
+							{
+								fields: {
+									isStandalone: false,
+									title: 'Some More Content',
+									images: [],
+									order: 2
+								}
+							},
+							{
+								fields: {
+									isStandalone: false,
+									title: 'Even More Content',
+									images: [],
+									order: 3
+								}
+							},
+							{
+								fields: {
+									isStandalone: true,
+									title: 'More Test Content',
+									images: [],
+									order: 4
+								}
+							}
+						]
+					}
+				});
+
+				/**
+				 *	Dummy data
+				 */
+				var data = {
+					page: 'test'
+				};
+
+				/**
+				 *	Call the helper function and run the tests
+				 */
+				var result = Template.views_content.__helpers[' groupedContentItems'].call(data);
+
+				expect(result.length).toEqual(3);
+				expect(result[0] instanceof Array).toBe(true);
+				expect(result[0].length).toEqual(2);
+				expect(result[1] instanceof Object).toBe(true);
+				expect(result[2] instanceof Object).toBe(true);
+
+				/**
+				 *	Finished
+				 */
+				done();
+			});
+
+		});
 
 	});
 
