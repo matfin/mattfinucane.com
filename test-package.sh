6af676dfe60f6aa50541cd4543db05eef0ade722
Matt Finucane <matfin@gmail.com>
2015-08-14 15:27:59 +0200

    Removed some entries from .gitignore. Adding travis config file for testing. Adding test scripts.

---
+++ test-package.sh
@@ -0,0 +1,1 @@
+VELOCITY_TEST_PACKAGES=1 JASMINE_CLIENT_INTEGRATION=0 meteor test-packages --once --driver-package velocity:console-reporter --velocity --release velocity:METEOR@1.1.0.3_1 ./packages/core
