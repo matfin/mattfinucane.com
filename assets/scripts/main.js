524eb5585a2e2666584b8b36c99f23eecbdb21da
Matt Finucane <matfin@gmail.com>
2017-05-04 12:02:02 +0200

    Set up gulp to concatenate scripts. Set up menu reveal toggle buttons.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,18 @@
-(() => {
-	console.log('Matt Finucane');
-})();
+const primeTapEvent = (selector, fn) => {
+	const items = document.querySelectorAll(selector);
+	items.forEach((item) => {
+		if('onpointerdown' in window) {
+			item.addEventListener('pointerdown', fn);
+		}
+		else if('ontouchstart' in window) {
+			item.addEventListener('touchstart', fn);
+		}
+		else {
+			item.addEventListener('click', fn);	
+		}
+	});
+};
+
+onload = () => {
+	primeTapEvent('header button', toggleNavReveal);
+};
