ea1883e5858651a9a314d6b6cea181adff5c75b4
Matt Finucane <matfin@gmail.com>
2017-05-08 17:32:15 +0200

    Styling and behaviour for the header nav.

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,14 +1,14 @@
 const toggleNavReveal = () => {
-
-	console.log('Tapped the nav reveal?');
-
 	let body 	= document.body,
+		header	= document.querySelector('header'),
 		nav 	= document.querySelector('nav');
 
 	if(nav.classList.toggle('revealed')) {
+		header.classList.add('revealed');
 		body.classList.add('prevent-scroll');
 	}
 	else {
+		header.classList.remove('revealed');
 		body.classList.remove('prevent-scroll');
 	}
 };
