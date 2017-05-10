53be7bf68292781b164dec295c9e9eeffcbcb078
Matt Finucane <matfin@gmail.com>
2017-05-10 11:31:02 +0200

    Adding SVG icons for the menu buttons.

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,14 +1,14 @@
 const toggleNavReveal = () => {
 	let body 	= document.body,
 		header	= document.querySelector('header'),
-		nav 	= document.querySelector('nav');
+		nav 	= header.querySelector('nav');
 
 	if(nav.classList.toggle('revealed')) {
-		header.classList.add('revealed');
+		header.classList.add('nav-open');
 		body.classList.add('prevent-scroll');
 	}
 	else {
-		header.classList.remove('revealed');
+		header.classList.remove('nav-open');
 		body.classList.remove('prevent-scroll');
 	}
 };
